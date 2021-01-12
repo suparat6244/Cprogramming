2afcede2142ba6deb8fa6c87634ae1f051bca87a
suparat6244 <s6302041610065@email.kmutnb.ac.th>
2021-01-12 16:00:39 +0700

    first commit

---
+++ Addition.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// ดึง library Input Output
+
+void main(){
+ // ส่วนที่ 2 Function หลักของภาษา C คือ  Main
+
+//-----------คำสั่งฌปรแกรมบวกเลย------------//
+    int num1, num2, total;
+    //  ประกาศตัวแปร num 1 num 2 และ total
+
+    printf("value of num1 is : ");
+    scanf("%d" ,&num1);
+    printf("value of nmu2 is : ");
+    scanf("%d" ,&num2);
+    total= num1 + num2;
+    printf("Total is : %d" ,total);
+
+    //--------สิ้นสุดโปรแกรมบวกเลข----------
+ }
---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// ส่วนสำหรับตึง Library (1)
+// การกด ctrl + / คือการใส่คอมเมนต์แบบเร็ว short cut key
+
+// ส่วน function หลัก (2)
+int main()
+{
+    printf("Suparat poonsawat\nearn\n19\ncat\nfood\neat delicious food");
+    /*
+    ส่วนคำสั่งการทำงาน (3)
+        printf คือ ฟังก์ชันที่ไว้ใช้สำหรับแสดงผลทางหน้าจอ
+    */
+    // วิธีการจัดชุดคำสั่งให้สวย ให้กด shfit + alt + function
+    // การเปิด Terminal ให้กด ctrl + j
+}
